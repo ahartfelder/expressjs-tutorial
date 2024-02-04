ba037f38992526d64586683759c07b4f4e19795f
Andreas Hartfelder <ahartfelder@gmail.com>
2024-02-04 19:43:49 -0300

    basic groceries app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const groceriesList = [
+  {
+    item: "Apple",
+    quantity: 5
+  },
+  {
+    item: "Grapes",
+    quantity: 2
+  },
+  {
+    item: "Pineapple",
+    quantity: 1
+  }
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  if (Object.keys(req.body).length) console.log(req.body);
+  next();
+});
+
+app.get("/groceries", (req, res) => {
+  res.send(groceriesList);
+});
+
+app.post ("/groceries", (req, res) => {
+  groceriesList.push(...req.body);
+  res.status(201).send(groceriesList);
+});
+
+app.listen(PORT, () => `Running Express Server on port ${PORT}`);
